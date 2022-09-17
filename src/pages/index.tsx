942ac233419ebfdd7a13bf52eefb490506b30a34
ShadeDev7 <shadedev7@gmail.com>
2022-09-17 11:33:34 -0300

    Project Initialization

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Index: NextPage = () => {
+    return (
+        <>
+            <h1>Hello World!</h1>
+        </>
+    );
+};
+
+export default Index;
